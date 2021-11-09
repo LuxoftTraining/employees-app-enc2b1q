e0bc1d5c082abbf0d3d0d7406ef23dbc0488cc96
enc2b1q <enc2b1q@example.com>
2021-11-09 07:57:30 +0300

    Блок 1. Занятие 3.

--- service.js
+++ service.js
@@ -46,3 +46,79 @@ function showEmployees() {
     }
 }   
 
+const employeeMap = {};
+
+function findById(id) {
+    if (employeeMap[id]) {
+        return employeeMap[id];
+    }
+    for (var e of DATA.employees) {
+        if (id==e.id) {
+            employeeMap[id] = e;
+            return e;
+        }
+    }
+}
+
+function addPhone(id, phone) {
+    const employee = findById(id);
+    const phones = employee.phones;
+    if (!phones) {
+        employee.phones = [];
+    }
+    employee.phones.push(phone);
+}
+
+function setDateOfBirth(id, date) {
+    const employee = findById(id);
+    employee.dateOfBirth = date;
+}
+
+function getAge(id) {
+    const employee = findById(id);
+    let ageDiff = Date.now() - employee.dateOfBirth.getTime();
+    let ageDate = new Date(ageDiff); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function formatDate(date) {
+    let day = date.getDate();
+    if (day<10) day = '0'+day;
+        let month = date.getMonth()+1;
+    if (month<10) month = '0'+month;
+        let year = date.getFullYear();
+
+    return day + '.' + month + '.' + year;
+}
+   
+function getEmployeeInfo(id) {
+    const e = findById(id);
+   
+    const phones = e.phones?
+        `Список телефонов: ${e.phones}`:'';
+    const age = e.dateOfBirth?
+        `Возраст: ${getAge(e.id)}`:'';
+    return ` 
+     Имя: ${e.name}
+     Фамилия: ${e.surname}
+     Дата рождения: ${formatDate(e.dateOfBirth)}
+     ${phones} 
+     ${age}
+    `;
+}
+   
+function testEmployee() {
+    addPhone(2, "555-55-55");
+    addPhone(2, "666-66-66");
+    setDateOfBirth(2, new Date(2000,1,1))
+    const info = getEmployeeInfo(2);
+    console.log(info);
+}
+   
+function getEmployeeJSON(id) {
+    const e = findById(id);
+    return JSON.stringify(e);
+}
+   
+
+
